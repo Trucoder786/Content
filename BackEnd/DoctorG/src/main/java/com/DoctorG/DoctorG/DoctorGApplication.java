fcc5728a7fef4c85c4eec0489033f3f8064dbdc7
Divyanshu <trucoder786@gmail.com>
2024-05-26 19:22:21 +0530

    Creating Normal Springboot and angular projects

---
+++ BackEnd/DoctorG/src/main/java/com/DoctorG/DoctorG/DoctorGApplication.java
@@ -0,0 +1,13 @@
+package com.DoctorG.DoctorG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorGApplication.class, args);
+	}
+
+}
